5f9acc0b0a92e6b6739413685570c82a1ed35bf2
minju0726 <minju0726@gmail.com>
2024-03-11 16:30:15 +0900

    완료

---
+++ js/main.js
@@ -0,0 +1,190 @@
+window.onload = function () {
+    // 안내창 스크립트
+const body = document.querySelector("body");
+const modal = document.querySelector(".modal-wrap");
+
+// isOpen 값에 따라 스크롤을 제어하는 함수
+function controlScroll(isOpen) {
+  if (isOpen) {
+    body.style.overflow = "hidden";
+  } else {
+    body.style.overflow = "auto";
+  }
+}
+
+// 초기 모달 상태 설정
+const isOpen = true;
+controlScroll(isOpen);
+
+modal.addEventListener("click", function () {
+  modal.style.display = "none";
+ 
+  // 모달이 닫힐 때는 스크롤을 다시 활성화
+  controlScroll(false);
+});
+
+  // <!-- promotion swiper -->
+  var swiper = new Swiper(".sw-promotion", {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    speed: 1000,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".promotion .sw-next",
+      prevEl: ".promotion .sw-prev",
+    },
+    pagination: {
+      el: ".sw-promotion-pg",
+      clickable: true,
+    },
+    breakpoints: {
+      760: {
+        slidesPerView: 2,
+      },
+    },
+  });
+  //  shopping swiper
+  var swiper = new Swiper(".sw-shopping", {
+    slidesPerView: 5,
+    grid: {
+      rows: 2,
+      fill: "row",
+    },
+    spaceBetween: 10,
+    breakpoints: {
+      900: {
+        spaceBetween: 32,
+        slidesPerView: 4,
+        // 화면당 3개씩 슬라이드 이동
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
+      },
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        // 화면당 3개씩 슬라이드 이동
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 4,
+        // 화면당 4개씩 슬라이드 이동
+        slidesPerGroup: 4,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+  });
+  //  Tour Swiper
+  var swiper = new Swiper(".sw-tour", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    grid: {
+      rows: 2,
+      fill: "row",
+    },
+    loop: true,
+    navigation: {
+      nextEl: ".tour .sw-next",
+      prevEl: ".tour .sw-prev",
+    },
+    breakpoints: {
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 2,
+        // 화면당 2개씩 슬라이드 이동
+        slidesPerGroup: 2,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        spaceBetween: 26,
+        slidesPerView: 3,
+        // 화면당 4개씩 슬라이드 이동
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+  });
+  //   ticket swiper
+  var swiper = new Swiper(".sw-ticket", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    breakpoints: {
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 32,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 27,
+      },
+    },
+  });
+  //   live swiper
+  var swiper = new Swiper(".sw-live", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 32,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 27,
+      },
+    },
+  });
+  //   books swiper
+  var swiper = new Swiper(".sw-books", {
+    slidesPerView: 3,
+    spaceBetween: 19,
+    grid:{
+      rows:4,
+      fill:"row"
+    }
+  });
+  //   events swiper
+  var swiper = new Swiper(".sw-events", {
+    slidesPerView: 3,
+    spaceBetween: 27,
+    breakpoints: {
+      1280: {
+        slidesPerView: 4,
+      },
+    },
+  });
+  // go top 버튼
+
+  // 비주얼 이동 버튼 스크롤 기능
+  const button = document.querySelector(".gotop");
+  // 비주얼 클릭했을때 스크롤이 이동하는 함수를 실행한다.
+  button.addEventListener("click", function () {
+    // button클릭했을때, 실행해라 . scrolltoSection()을
+    // scrolltoSection()#business 영역으로 이동 해라
+    scrolltoSection("#header");
+  });
+  function scrolltoSection(sectionId) {
+    // sectionId로 지정된 요소를 찾아서 section 변수에 저장
+    const section = document.querySelector(sectionId);
+    if (section) {
+      // scrollIntoView()메서드는 부드러운 스크롤 효과를 적용하도록 지정합니다.
+      section.scrollIntoView({ behabior: "smooth" });
+    }
+  }
+};
+
